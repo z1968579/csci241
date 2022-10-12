774fffba8b1af52a85f7f4b4bf517565cdc4a332
Jake Kurbis <z1945650@turing.cs.niu.edu>
2022-10-11 23:44:44 -0500

    Added Assign6 dir and files

---
+++ Assign6/matrix.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file matrix.cpp
+ * @author Jacob Rudawski (z1968579) & Jacob Kurbis (z1945650)
+ * @brief CSCI 330 Assignment 6
+ * @date 2022-10-13
+ * 
+ */
+
+#include <iostream>
+#include "matrix.h"
+
+using std::cout;
+using std::endl;
+
+
+int matrix::determinant() const
+{
+}
+
+
+matrix matrix::operator+(const matrix&) const
+{
+}
+
+
+matrix matrix::operator*(const int&) const//reference to int "argument"& in this one
+{
+}
+
+
+matrix matrix::operator*(const matrix&) const//reference to matrix_object "argument"& in this one
+{
+}
+
+
+bool matrix::operator==(const matrix&) const
+{
+}
+
+
+bool matrix::operator!=(const matrix&) const
+{
+}
+
+
+friend matrix::ostream& operator<<(ostream &, const matrix&)
+{
+}
+
+
+friend matrix::matrix operator*(const int&, const matrix&)
+{
+}
---
+++ Assign6/matrix.h
@@ -0,0 +1,47 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+/**
+ * @file book.h
+ * @author Jacob Rudawski (z1968579) & Jacob Kurbis (z1945650)
+ * @brief CSCI 330 Assignment 6
+ * @date 2022-10-13
+ */
+
+class matrix
+{
+    int matrix_array[2][2];
+
+    public:
+        //Constructors
+        identify_matrix();
+        array_intialization(int[][]);
+
+        //Methods
+
+        /////////////
+        // The Key ////////////////  
+        /////////////       //  //
+
+        // a "reference" is the "&" symbol, "matrix" is a class object,
+        // "const methods or functons" are declared with a "const" at end of the prototype,
+        // "overloaded operators" are established by typing "operator" followed by whatever 
+        // operator symbol you want to overload(+, <<, *, etc.)
+
+        int determinant() const;
+        matrix operator+(const matrix&) const;
+        matrix operator*(const int&) const;//reference to int "argument"& in this one
+        matrix operator*(const matrix&) const;//reference to matrix_object "argument"& in this one
+        bool operator==(const matrix&) const;
+        bool operator!=(const matrix&) const;
+        friend ostream& operator<<(ostream &, const matrix&);
+        friend matrix operator*(const int&, const matrix&);
+
+        //***Check these class methods out and double check if they are prototyped correctly please!***
+        //***If any are wrong the implementation headers of these prototypes in the matrix.cpp file ***
+        //***will have to be changed!                                                               ***
+        //
+        //                                          Thankyou!
+
+};
+#endif
+#endif
