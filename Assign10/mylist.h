e99f261f851d0670fe6af5534d6bc555f72b0308
Jake Kurbis <z1945650@turing.cs.niu.edu>
2022-11-10 14:05:02 -0600

    Add Assign 10 directory and files

---
+++ Assign10/mylist.h
@@ -0,0 +1,14 @@
+#ifndef MYLIST_H
+#define MYLIST_H
+/**
+ * @file mylist.h
+ * @author Jacob Rudawski (z1968579) & Jacob Kurbis (z1945650)
+ * @brief CSCI 241 Assignment 10
+ * @date 11-16-2022
+ */
+
+#include <iostream>
+#include <stdexcept>
+
+
+ #endif
